6ec026935172dcff6768e1809ab1c5b7d6cff600
Gb16702 <delgeoffrey1@gmail.com>
2023-09-17 08:09:23 +0200

    Server Init (main + loadEnv func and go mod)

---
+++ server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"fmt"
+	"os"
+	"log"
+)
+
+type environment struct {
+	Url string
+}
+
+/**
+*	Main function
+*
+*	@returns void
+*/
+func main() {
+	env, err := loadEnv()
+	if err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
+
+	fmt.Println("Server running on port: " + env.Url)
+}
+
+/**
+*	Load environment variables from .env file
+*
+*	@returns environment struct
+*	@returns error
+*/
+func loadEnv() (environment, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
+
+	env := environment{
+		Url: os.Getenv("SERVER_URL"),
+	}
+
+	return env, nil
+}
