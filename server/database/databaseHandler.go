21c215e0c5abb7b1a75f464c7c490be5fc983c8d
Gb16702 <delgeoffrey1@gmail.com>
2023-09-18 05:02:52 +0200

    Created User model and implemented migrations

--- server/database/databaseHandler.go
+++ server/database/databaseHandler.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"host/database/models"
 )
 
 var DB *gorm.DB;
@@ -17,6 +18,12 @@ var DB *gorm.DB;
 *	@returns void
 */
 func ConnectToDatabase(connectionString string) {
+	defer func() {
+		if err := CloseDatabaseConnection(DB); err != nil {
+			log.Fatalf("Error while closing database connection: %v", err)
+		}
+	}()
+
 	var err error;
 	DB, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
@@ -25,17 +32,44 @@ func ConnectToDatabase(connectionString string) {
 	if err != nil {
 		log.Fatalf("Error while connecting to database: %v", err)
 	}
-	
+
 	log.Println("Successfully connected to database");
+
+	message, err := migrateSchemas(DB);
+	if err != nil {
+		log.Fatalf("Error while migrating schemas: %v", err)
+	}
+
+	log.Println(message);
 }
 
 /**
 *	This function migrates the schemas
 *
-*	@param *gorm.DB DB
+*	@param *gorm.DB database
+*	@returns string
 *	@returns error
 */
-func migrateSchemas(DB *gorm.DB) error {
+func migrateSchemas(database *gorm.DB) (string, error) {
+	if err := database.AutoMigrate(&models.User{}); err != nil {
+		return "", err;
+	}
+	return "Successfully migrated schemas", nil;
+}
+
+/**
+*	This function closes the database connection
+*
+*	@param *gorm.DB database
+*	@returns error
+*/
+func CloseDatabaseConnection(database *gorm.DB) error  {
+	db, err := database.DB();
+	if err != nil {
+		return err;
+	}
+
+	db.Close();
 
 	return nil;
 }
---
+++ server/database/models/User.go
@@ -0,0 +1,18 @@
+package models
+
+type User struct {
+	ID 			uint 	`gorm:"primaryKey" json:"id"`
+	FirstName 	string 	`gorm:"not null json:firstname" `
+	LastName 	string 	`gorm:"not null" json:"lastname"`
+	CreatedAt 	int64
+	UserLogin UserLogin `gorm:"embedded"`
+};
+
+type UserLogin struct {
+	Email 		string 	`gorm:"unique" json:"email"`
+	Password 	string 	`gorm:"not null" json:"password"`
+}
+
+type Users struct {
+	Users []User `json:"users"`
+};
